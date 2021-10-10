1d8522f384389699f2a5346621928003a41e58bf
ayj84 <ayj84@DESKTOP-9BVNURB>
2021-10-11 00:25:28 +0900

    댓글 작성 추가

--- board/src/main/java/com/board/controller/ReplyController.java
+++ board/src/main/java/com/board/controller/ReplyController.java
@@ -4,7 +4,9 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.board.domain.ReplyVO;
 import com.board.service.ReplyService;
 
 @Controller
@@ -12,12 +14,23 @@ import com.board.service.ReplyService;
 public class ReplyController {
 
 	@Inject
-	private ReplyService service;
+	private ReplyService replyService;
+	// 1. 게시물 조회 페이지에서 댓글 작성후 등록
+	// 2. /reply/write 에 해당되는 컨트롤러오 데이터 전달
+	// 3. service => dao => mapper를 통해 db에 전달
+	// 4. redirect:/board/view?bno= 로인해 1번에서 조회중이었던 게시물로 이동
+	
 	
 	// 댓글 조회
 	
 	// 댓글 작성
-	
+	@RequestMapping(value = "/write", method = RequestMethod.POST)
+	public String posttWirte(ReplyVO vo) throws Exception {
+		
+		replyService.write(vo);
+		
+		return "redirect:/board/view?bno=" + vo.getBno();
+	}
 	// 댓글 수정
 	
 	// 댓글 삭제
